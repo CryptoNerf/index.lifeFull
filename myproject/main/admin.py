09ae3e469bb45042a9a04102549539af640ef2dc
CryptoNerf <anuta1711_98@mail.ru>
2025-09-12 01:55:20 +0400

    change admin and delete old photo

--- myproject/main/admin.py
+++ myproject/main/admin.py
@@ -1,5 +1,42 @@
 from django.contrib import admin
-from .models import MoodEntry
+from .models import MoodEntry, Profile
 
-# Register your models here.
-admin.site.register(MoodEntry)
+@admin.register(MoodEntry)
+class MoodEntryAdmin(admin.ModelAdmin):
+    list_display = ['get_username', 'date', 'rating', 'get_short_note']
+    list_filter = ['rating', 'date', 'user']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'note']
+    ordering = ['-date']
+    date_hierarchy = 'date'
+    list_per_page = 25
+    
+    def get_username(self, obj):
+        full_name = f"{obj.user.first_name} {obj.user.last_name}".strip()
+        if full_name:
+            return f"{obj.user.username} ({full_name})"
+        return obj.user.username
+    get_username.short_description = 'Пользователь'
+    
+    def get_short_note(self, obj):
+        if not obj.note:
+            return "Без заметки"
+        return obj.note[:50] + "..." if len(obj.note) > 50 else obj.note
+    get_short_note.short_description = 'Заметка'
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['get_username', 'get_email', 'has_photo']
+    search_fields = ['user__username', 'user__email']
+    
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = 'Пользователь'
+    
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
+    
+    def has_photo(self, obj):
+        return bool(obj.photo)
+    has_photo.boolean = True
+    has_photo.short_description = 'Есть фото'
--- myproject/main/models.py
+++ myproject/main/models.py
@@ -1,4 +1,5 @@
 # main/models.py
+import os
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -19,6 +20,28 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile of {self.user.username}"
+    
+    def save(self, *args, **kwargs):
+        # Если это обновление существующего профиля
+        if self.pk:
+            try:
+                old_profile = Profile.objects.get(pk=self.pk)
+                # Если старая фотка существует и новая фотка отличается от старой
+                if old_profile.photo and old_profile.photo != self.photo:
+                    # Удаляем старый файл с диска
+                    if os.path.isfile(old_profile.photo.path):
+                        os.remove(old_profile.photo.path)
+            except Profile.DoesNotExist:
+                pass
+        
+        super().save(*args, **kwargs)
+    
+    def delete(self, *args, **kwargs):
+        # Удаляем файл фото при удалении профиля
+        if self.photo:
+            if os.path.isfile(self.photo.path):
+                os.remove(self.photo.path)
+        super().delete(*args, **kwargs)
 
 # Automatically create or update Profile when User is saved
 @receiver(post_save, sender=User)
